440561192ea7f189696e15dcd32d0310691fb135
whitealcea <44871864+whitealcea@users.noreply.github.com>
2020-03-22 17:38:01 +0900

    export secret key

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+from .local_settings import *
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -20,7 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'av_vc*eq3d276c8+=0wxgqm3aeh7pucsi$6p#mzrjq*(2l&)tq'
+SECRET_KEY = LOCAL_SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
